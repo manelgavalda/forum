3dddecc842f63041393a546d9697cfb55ffd58e9
Manel Gavaldà Andreu <expedientmanel@gmail.com>
2018-06-13 22:39:15 +0200

    Validation errors and old data

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,26 +1,40 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Create new Thread</div>
-
                 <div class="card-body">
                     <form method="POST" action="/threads">
                         @csrf
                         <div class="form-group">
-                            <label for="title">Title</label>
-                            <input type="text" class="form-control" id="title" name="title">
+                            <label for="channel_id">Chose a Channel</label>
+                            <select name="channel_id" value="{{ old('channel_id') }}" class="form-control" required>
+                                <option value="">Chose One...</option>
+                                @foreach(App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}" {{old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                @endforeach
+                            </select>
                         </div>
-
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}" required>
+                        </div>
                         <div class="form-group">
                             <label for="body">Body</label>
-                            <textarea name="body" id="body" class="form-control" rows="8"></textarea>
+                            <textarea id="body" class="form-control" name="body" value="{{ old('body') }}" rows="8" required></textarea>
                         </div>
-
-                        <button type="submit" class="btn btn-primary">Publish</button>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Publish</button>
+                        </div>
+                        @if(count($errors))
+                        <ul class="alert alert-danger">
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
                     </form>
                 </div>
             </div>
