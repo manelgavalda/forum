93ef46c5c1d694cfe2b5624ea512fa71d7135bd8
Manel Gavaldà Andreu <expedientmanel@gmail.com>
2018-07-15 22:20:05 +0200

    Meta details and pagination

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,15 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyGuest', function ($builder) {
+            $builder->withCount('replies');
+        });
+    }
+
     public function path()
     {
         return "/threads/{$this->channel->slug}/{$this->id}";
@@ -16,6 +25,11 @@ class Thread extends Model
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function getReplyCountAttribute()
+    {
+        return $this->replies()->count();
     }
 
     public function creator()
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,32 +13,33 @@
                     <div class="body">{{ $thread->body }}</div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                @foreach($thread->replies as $reply)
-                    @include('threads.reply')
-                @endforeach
-            </div>
-        </div>
-    </div>
-    @if (auth()->check())
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <form method="post" action="{{ "{$thread->path()}/replies" }}">
-                        @csrf
-                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+            @foreach($replies as $reply)
+                @include('threads.reply')
+            @endforeach
 
-                        <button type="submit" class="btn btn-default">Post</button>
-                    </form>
+            {{ $replies->links() }}
+
+            @if (auth()->check())
+                <form method="post" action="{{ "{$thread->path()}/replies" }}">
+                    @csrf
+                    <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+
+                    <button type="submit" class="btn btn-default">Post</button>
+                </form>
+            @else
+                <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in the discussion.</p>
+            @endif
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <p>
+                        Thsi thread was published {{ $thread->created_at->diffForHumans() }} by
+                        <a href="#">{{ $thread->creator->name }}</a>, and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                    </p>
                 </div>
             </div>
         </div>
-    @else
-        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in the discussion.</p>
-    @endif
+    </div>
 </div>
 @endsection
