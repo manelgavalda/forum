ccd7ea4584d87796f4f86b1dec101e29e2335979
Manel Gavaldà Andreu <expedientmanel@gmail.com>
2018-07-20 22:42:00 +0200

    Added favorite button

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,20 @@
-<div class="card-header">
-    <a href="#">
-        {{ $reply->owner->name }}
-    </a> said {{ $reply->created_at->diffForHumans() }}...
-</div>
-<div class="card-body">
-    <p>{{ $reply->body }}</p>
+<div class="card" >
+    <div class="card-header">
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
+            </h5>
+            <div>
+                <form method="POST" action="/replies/{{$reply->id}}/favorites">
+                    @csrf()
+                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="body">{{ $reply->body }}</div>
+    </div>
 </div>
