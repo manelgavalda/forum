465c74e9f64c63de8f3b5a2366b46cd8fc8f88d9
Manel Gavaldà Andreu <expedientmanel@gmail.com>
2018-10-13 15:28:05 +0200

    First Class Search: Forum Integration

---
+++ resources/views/threads/search.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <ais-index
+            app-id="{{ config('scout.algolia.id') }}"
+            api-key="{{ config('scout.algolia.key') }}"
+            index-name="threads"
+            query="{{ request('q') }}"
+        >
+            <div class="row justify-content-center">
+
+                <div class="col-md-8">
+                    <ais-results>
+                        <template scope="{ result }">
+                            <li>
+                                <a :href="result.path">
+                                    <ais-highlight :result="result" attribute-name="title"></ais-highlight>
+                                </a>
+                            </li>
+                        </template>
+                    </ais-results>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Search
+                        </div>
+                        <div class="card-body">
+                            <ais-search-box>
+                                <ais-input placeholder="Find a thread..." class="form-control" :autofocus="true"></ais-input>
+                            </ais-search-box>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            Filter by channel
+                        </div>
+                        <div class="card-body">
+                            <ais-refinement-list attribute-name="channel.name"></ais-refinement-list>
+                        </div>
+                    </div>
+
+                    @if(count($trending))
+                        <div class="card">
+                            <div class="card-header">
+                                Trending Threads
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    @foreach($trending as $thread)
+                                        <li>
+                                            <a href="{{ url($thread->path) }}">
+                                                {{ $thread->title }}</li>
+                                            </a>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </ais-index>
+    </div>
+@endsection
