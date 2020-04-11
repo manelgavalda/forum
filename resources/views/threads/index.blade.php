f8e8c2f0265eb6f9d703d1ce39534aae2d4f0d3e
Manel Gavaldà Andreu <manelgavalda1@gmail.com>
2020-04-11 14:49:08 +0200

    Reddit like post list

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,39 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="m-auto">
-		@forelse($threads as $thread)
-			<div class="rounded shadow-lg my-4 p-4 hover:bg-gray-200">
-		    <div class="font-bold text-xl">
+	@forelse($threads as $thread)
+		<div class="my-2 py-1 pl-10 rounded-sm cursor-pointer border border-blue-500 hover:border-yellow-100">
+	    	<div class="text-xs text-gray-600">
+	    		Posted by
+		    	<a
+		    		class="hover:underline"
+		    		href="{{ route('profile', $thread->creator) }}"
+	    		>
+		    		{{ $thread->creator->name }}
+		    	</a>
+		    	{{ $thread->created_at->diffForHumans() }}
+	    	</div>
+		    <div class="font-semibold text-xl">
 					<a href="{{ $thread->path() }}">
 						{{ $thread->title }}
 					</a>
 		    </div>
-		    <div class="border"></div>
-		    <div class="flex py-3 text-xs">
-		    	<div>
-							{{ $thread->replies_count }}
-							{{ Illuminate\Support\Str::plural('reply', $thread->replies_count) }}
-					</div>
-		    	<div class="ml-auto">
-		    		Post by:
-			    	<a
-			    		class="font-bold underline"
-			    		href="{{ route('profile', $thread->creator) }}"
-		    		>
-			    		{{ $thread->creator->name }}
-			    	</a>
-		    	</div>
-		    </div>
-		    <div class="text-gray-700 text-sm">
+		    <div class="text-sm pt-2">
 		    	{{ $thread->body }}
 		    </div>
-		  </div>
-		@empty
-			<p>There are no relevant results at this time.</p>
-		@endforelse
+		    <div class="flex pt-1 text-xs font-bold text-gray-600">
+		    	<div class="hover:bg-gray-300 p-1 rounded-xs">
+		    		<i class="fa fa-user-o" aria-hidden="true"></i>
+					{{ $thread->replies_count }}
+					{{ Illuminate\Support\Str::plural('Comment', $thread->replies_count) }}
+		    	</div>
+		    </div>
+	  	</div>
+	@empty
+		<p>There are no relevant results at this time.</p>
+	@endforelse
 
-		{{ $threads->render() }}
-
-	</div>
+	{{ $threads->render() }}
 @endsection
