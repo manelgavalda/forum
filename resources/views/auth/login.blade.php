12e764011429f7e0e24dc485ab640a7e0d51aeab
Manel Gavaldà Andreu <manelgavalda1@gmail.com>
2020-04-18 14:02:59 +0200

    login page style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,74 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="w-4/6 m-auto shadow-lg border">
-		<div class="p-3 bg-gray-200 border-2 text-center">Login</div>
-
-		<div class="p-3">
-			<form method="POST" action="{{ route('login') }}">
-				@csrf
-
-				<div class="pb-3 flex">
-					<label class="w-2/6 mt-2 text-right">Email</label>
-
-					<div class="col-md-6">
-						<input id="email" type="email"
-							class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-							name="email" value="{{ old('email') }}" required autofocus
-						>
-
-						@if ($errors->has('email'))
-							<span class="invalid-feedback">
-								<strong>{{ $errors->first('email') }}</strong>
-							</span>
-						@endif
-					</div>
-				</div>
-
-				<div class="pb-3 flex">
-					<label class="w-2/6 mt-2 text-right">Password</label>
-
-					<div class="col-md-6">
-						<input id="password" type="password"
-							class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-							name="password" required
-						>
-
-						@if ($errors->has('password'))
-							<span class="invalid-feedback">
-								<strong>{{ $errors->first('password') }}</strong>
-							</span>
-						@endif
-					</div>
-				</div>
-
-				<div class="pb-3 flex">
-					<div class="m-auto">
-						<div class="checkbox">
-							<label>
-								<input type="checkbox" name="remember"
-									class="form-checkbox border-2"
-									{{ old('remember') ? 'checked' : '' }}
-								> Remember me</label>
-						</div>
-					</div>
-				</div>
-
-				<div>
-					<div class="flex">
-						<div class="m-auto">
-							<button type="submit" class="p-2 px-3 border-2 rounded ">
-								{{ __('Login') }}
-							</button>
-
-							<a class="btn btn-link" href="{{ route('password.request') }}">
-								{{ __('Forgot Your Password?') }}
-							</a>
-
-						</div>
-					</div>
-				</div>
-			</form>
-		</div>
+	<div class="max-w-sm">
+	  	<form
+	  		class="bg-white border border-gray-400 rounded px-8 pt-6 pb-8 mb-4"
+	  		method="POST"
+	  		action="{{ route('login') }}"
+  		>
+	  		@csrf
+		    <div class="mb-4">
+		      	<label
+		      		class="block text-gray-700 text-sm font-bold mb-2"
+		      		for="username"
+	      		>
+		        	Username
+		      	</label>
+		      	<input
+		      		class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+		      		type="text"
+		      		placeholder="Email"
+		      		name="email"
+					value="{{ old('email') }}"
+				>
+				@error('email')
+		      		<p class="text-red-500 text-xs italic">
+						<strong>{{ $message }}</strong>
+					</p>
+				@enderror
+		    </div>
+		    <div class="mb-6">
+		      	<label
+			      	class="block text-gray-700 text-sm font-bold mb-2"
+			      	for="password"
+		      	>
+		        	Password
+		      	</label>
+		      	<input
+			      	class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+			      	type="password"
+			      	name="password"
+			      	placeholder="******************"
+		      	>
+				@error('password')
+		      		<p class="text-red-500 text-xs italic">
+						<strong>{{ $message }}</strong>
+					</p>
+				@enderror
+		    </div>
+		    <div class="flex items-center justify-between">
+		      	<button
+		      		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+		      		type="submit"
+	      		>
+		        	Sign In
+		      	</button>
+		      	<a
+		      		class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+		      		href="{{ route('password.request') }}"
+	      		>
+		        	Forgot Password?
+		     	</a>
+		    </div>
+	  	</form>
 	</div>
 @endsection
