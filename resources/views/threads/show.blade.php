adaba33205adea3bff0aa6365b651982b40d3707
Manel Gavaldà Andreu <manelgavalda1@gmail.com>
2020-04-18 14:25:47 +0200

    Better post page

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,32 +5,54 @@
 @endsection
 
 @section('content')
-	<thread-view :thread="{{ $thread }}" inline-template>
-		<div>
+	<thread-view
+		:thread="{{ $thread }}"
+		inline-template
+	>
+		<div class="rounded border border-gray-400 p-10">
 			<div>
 				{{-- Editing the question --}}
 				<div class="card" v-if="editing">
 					<div class="card-header">
 						<div class="level">
-							<input type="text" class="form-control" v-model="form.title">
+							<input
+								type="text"
+								class="form-control"
+								v-model="form.title"
+							>
 						</div>
 					</div>
 					<div class="card-body">
 						<div class="form-group">
-							<wysiwyg v-model="form.body"></wysiwyg>
+							<wysiwyg
+								v-model="form.body"
+							></wysiwyg>
 						</div>
 					</div>
 
 					<div class="card-footer">
 						<div class="level">
-							<button class="btn btn-primary level-item" @click="update">Update</button>
-							<button class="btn btn-xs level-item" @click="resetForm">Cancel</button>
+							<button
+								class="btn btn-primary level-item"
+								@click="update"
+							>Update</button>
+							<button
+								class="btn btn-xs level-item"
+								@click="resetForm"
+							>Cancel</button>
 							@can('update', $thread)
-								<form action="{{ $thread->path() }}" method="POST" class="ml-a">
+								<form
+									action="{{ $thread->path() }}"
+									method="POST"
+									class="ml-a"
+								>
 									@csrf
 									{{ method_field('DELETE') }}
 
-									<button type="submit" class="btn btn-link">Delete Thread</button>
+									<button
+										type="submit"
+										class="btn btn-link"
+									>Delete Thread</button>
 								</form>
 							@endcan
 						</div>
@@ -38,49 +60,69 @@
 				</div>
 
 				{{-- Viewing the question --}} --}}
-				<div class="rounded shadow-lg mb-4 p-4" v-else>
-					<div class="flex">
-						<div class="flex">
-							<img
-								src="{{ asset($thread->creator->avatar_path) }}"
-								alt="{{ $thread->creator->name }}"
-								width="25"
-							>
+				<div class="mb-10" v-else>
+					<div class="flex items-center">
+						<div class="text-sm font-bold mr-1">
 							<a
-								class="font-bold underline pl-2"
-								href="{{ route('profile', $thread->creator) }}"
+					    		class="hover:underline"
+								href="{{ route('threads', $thread->channel)}}"
 							>
-								{{ $thread->creator->name }}
+								r/{{ $thread->channel->name }}
 							</a>
+
 						</div>
-						<div class="ml-auto text-sm text-gray-600">
-							{{ $thread->created_at->diffForHumans() }}
-						</div>
-					</div>
+				    	<div class="text-xs text-gray-600">
+				    		• Posted by
+					    	<a
+					    		class="hover:underline"
+					    		href="{{ route('profile', $thread->creator) }}"
+				    		>
+					    		{{ $thread->creator->name }}
+					    	</a>
+					    	{{ $thread->created_at->diffForHumans() }}
+				    	</div>
+			    	</div>
 					<div class="font-bold text-xl">
-							<a href="{{ $thread->path() }}"> <span v-text="title"></span></a>
+						<a href="{{ $thread->path() }}">
+							<span v-text="title"></span>
+						</a>
 					</div>
-					<div class="border"></div>
-					<div class="flex py-3 text-xs">
-						<div>
+					<div class="text-sm pt-2">
+		    			{{ $thread->body }}
+				    </div>
+				    <div class="flex pt-1 text-xs font-bold text-gray-600">
+				    	<div class="hover:bg-gray-300 p-1 rounded-xs">
+				    		<i class="fa fa-comment" aria-hidden="true"></i>
 							{{ $thread->replies_count }}
-							{{ Illuminate\Support\Str::plural('reply', $thread->replies_count) }}
-						</div>
-					</div>
-					<div class="text-gray-700 text-sm" v-html="body"></div>
-					<div class="card-footer" v-if="authorize('owns', thread)">
-						<button class="btn btn-xs" @click="editing = true">Edit</button>
+							{{ Illuminate\Support\Str::plural('Comment', $thread->replies_count) }}
+				    	</div>
+				    </div>
+					<div
+						class="card-footer"
+						v-if="authorize('owns', thread)"
+					>
+						<button
+							class="btn btn-xs"
+							@click="editing = true"
+						>Edit</button>
 					</div>
 				</div>
-				<replies @added="repliesCount++" @removed="repliesCount--"></replies>
+				<replies
+					@added="repliesCount++"
+					@removed="repliesCount--"
+				></replies>
 			</div>
 			<p>
-				<subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}" v-if="signedIn"></subscribe-button>
+				<subscribe-button
+					:active="{{ json_encode($thread->isSubscribedTo) }}"
+					v-if="signedIn"
+				></subscribe-button>
 
 				<button class="btn btn-default"
 					v-if="authorize('isAdmin')"
 					@click="toogleLock"
-					v-text="locked ? 'Unlock' : 'Lock'"></button>
+					v-text="locked ? 'Unlock' : 'Lock'"
+				></button>
 			</p>
 		</div>
 	</thread-view>
