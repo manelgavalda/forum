e26ce96bfe9d3b7509c2b91992e03f64fdb63df3
Manel Gavaldà Andreu <manelgavalda1@gmail.com>
2020-04-18 20:15:38 +0200

    Better navbar for autenticated users

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -24,32 +24,41 @@
 		</div>
 		<div class="ml-auto my-auto">
 			@guest
-		      	<a href="{{ route('login') }}" class="text-xs font-bold py-1 px-2 border-2 rounded text-blue-600 border-blue-600 hover:border-blue-400 hover:text-blue-500">Log in</a>
-
-		      	<a href="{{ route('register') }}" class="text-xs font-bold py-1 px-2 border-2 rounded bg-blue-600 text-white border-blue-600 hover:bg-blue-400 hover:border-blue-400 hover:text-blue-500">Sign up</a>
+		      	<a
+		      		href="{{ route('login') }}"
+		      		class="text-xs font-bold py-1 px-2 border-2 rounded text-blue-600 border-blue-600 hover:border-blue-400 hover:text-blue-500 mr-2"
+	      		>
+		      		Log in
+		      	</a>
+		      	<a
+			      	href="{{ route('register') }}"
+			      	class="text-xs font-bold py-1 px-2 border-2 rounded bg-blue-600 text-white border-blue-600 hover:bg-blue-400 hover:border-blue-400 hover:text-blue-500"
+		      	>
+		      		Sign up
+		      	</a>
 			@else
 				<user-notifications></user-notifications>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
-						{{ auth()->user()->name }}
-					</a>
-
-					<div class="dropdown-menu">
-						<a class="dropdown-item" href={{ route('profile', auth()->user()) }}>
-							My Profile
-						</a>
-						<a class="dropdown-item" href="{{ route('logout') }}"
-						  onclick="event.preventDefault();
-						 	document.getElementById('logout-form').submit();"
-					 	>
-					 		Logout
-						</a>
-
-						<form id="logout-form" action="{{ route('logout') }}" method="POST">
-							@csrf
-						</form>
-					</div>
-				</li>
+				<a
+					href="{{ route('profile', auth()->user()) }}"
+					class="text-xs font-bold py-1 px-2 border-2 rounded text-blue-600 border-blue-600 hover:border-blue-400 hover:text-blue-500 mr-2"
+				>
+					{{ auth()->user()->name }}
+				</a>
+				<a
+					href="{{ route('logout') }}"
+					onclick="event.preventDefault();
+				 	document.getElementById('logout-form').submit();"
+					class="text-xs font-bold py-1 px-2 border-2 rounded bg-blue-600 text-white border-blue-600 hover:bg-blue-400 hover:border-blue-400 hover:text-blue-500"
+				>
+					Logout
+				</a>
+				<form
+					id="logout-form"
+					action="{{ route('logout') }}"
+					method="POST"
+				>
+					@csrf
+				</form>
 			@endguest
 		</div>
 	</div>
